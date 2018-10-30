5d7cf2b08a277b3a4b0212d73a5058801b1f3f26
Jaison DSouza B <jaisonj1010@gmail.com>
2018-10-30 23:05:48 +0530

    Add files via upload

---
+++ major/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var Converter = require("csvtojson").Converter;
+
+// create a new converter object
+var converter = new Converter({});
+
+// call the fromFile function which takes in the path to your 
+// csv file as well as a callback function
+converter.fromFile("./Contact Information.csv",function(err,result)
+{
+  console.log("hellaa");
+  if(err){
+    console.log("An Error Has Occured");
+    console.log(err);  
+  } 
+  if(typeof result==='undefined' || result===null)
+  {
+    console.log("null");
+  }
+    // result stored in variable json. 
+  
+    var json = result;
+    // log our json to verify it has worked
+    console.log(json);
+});
+app = express();
+
+const PORT= 3000;
+
+const server= app;
+
+server.listen(3000, ()=> {
+    console.log(`server started on port ${PORT}`);
+});
+
