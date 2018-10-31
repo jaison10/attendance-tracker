758acb3d0ff633aa3fc8297afad72c180ab24280
ggrao1 <ggrao1@users.noreply.github.com>
2018-10-31 21:28:57 +0530

    refer https://www.npmjs.com/package/csvtojson

--- major/app.js
+++ major/app.js
@@ -1,39 +1,23 @@
 var express = require('express');
-var Converter = require("csvtojson").Converter;
 
-// create a new converter object
-var converter = new Converter({});
+const csvFilePath="./Contact_Information.csv";
+const csv=require('csvtojson')
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj)=>{
+    console.log(jsonObj);
+})
 
-// call the fromFile function which takes in the path to your 
-// csv file as well as a callback function
-converter.fromFile("./Contact Information.csv",function(err,result)
-{
-  console.log("hellaa");
-  if(err){
-    console.log("An Error Has Occured");
-    console.log(err);  
-  } 
-  if(typeof result==='undefined' || result===null)
-  {
-    console.log("null");
-  }
-    // result stored in variable json. 
-  
-    var json = result;
-    // log our json to verify it has worked
-    console.log(json);
-});
-//const URL="localhost:";
 app = express();
 
 const PORT= 3000;
-
 const server= app;
-
 server.listen(3000, ()=> {
     console.log(`server started on port ${PORT}`);
 });
-
+server.get(`/`,(req,res)=>{
+    console.log("GOT IT");
+});
 server.get(`/:id`,(req,res)=>{
   const searchstudent= _.find(json, json => json.id===req.params.id)
   if(searchstudent)
